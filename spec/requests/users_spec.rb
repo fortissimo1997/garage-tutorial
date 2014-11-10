0026a023cb2cb28563c33c634d85dc3e12f423f4
fortissimo1997 <fortissimo1997@gmail.com>
2014-11-10 12:49:27 +0900

    Add test PUT /v1/users/:id
    - add some methods to UsersController

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,4 +11,26 @@ RSpec.describe 'users', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'PUT /v1/users/:user_id' do
+    before { params[:name] = 'bob' }
+
+    context 'with owned resource' do
+      let!(:user) { resource_owner }
+
+      it 'updates owned resource' do
+        put "/v1/users/#{user.id}", params, env
+        expect(response).to have_http_status(204)
+      end
+    end
+
+    context 'without owned resource' do
+      let!(:other) { create(:user, name: 'raymonde') }
+
+      it 'returns 403' do
+        put "/v1/users/#{other.id}", params, env
+        expect(response).to have_http_status(403)
+      end
+    end
+  end
 end
